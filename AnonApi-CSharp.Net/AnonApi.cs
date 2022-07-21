6d5c8aac7f37744d3d8949b84fb3baebe97128ed
Ploch <Ploch@LENOVO>
2022-07-21 12:15:17 +0200

    Dodaj pliki projektów.

---
+++ AnonApi-CSharp.Net/AnonApi.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Net;
+using Newtonsoft.Json;
+
+namespace AnonApi_CSharp.Net
+{
+    public class AnonResults
+    {
+        [JsonProperty("status")] public bool Status { get; set; }
+        [JsonProperty("data")] public Data Data { get; set; }
+    }
+    public class Data
+    {
+        [JsonProperty("file")] public file file { get; set; }
+    }
+    public class file
+    {
+        [JsonProperty("url")] public url url { get; set; }
+    }
+    public class url 
+    {
+        [JsonProperty("full")] public string full { get; set; }
+    }
+    internal class AnonApi
+    {
+        public AnonApi() { }
+        public AnonResults sendFile(string filepath)
+        {
+            WebClient client = new WebClient();
+            return JsonConvert.DeserializeObject<AnonResults>(System.Text.Encoding.ASCII.GetString(client.UploadFile("https://api.anonfiles.com/upload", filepath)));
+        }
+    }
+}
---
+++ AnonApi-CSharp.Net/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnonApi_CSharp.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnonApi anonApi = new AnonApi();
+            Console.WriteLine(anonApi.sendFile(@"").Data.file.url.full);
+            Thread.Sleep(5000);
+        }
+    }
+}
